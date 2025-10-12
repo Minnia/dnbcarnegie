26819e6e42d383387da7ef37fcb2bc5eb48c9cd7
Minnia <jasminestockholm@gmail.com>
2025-10-12 22:06:08 +0200

    add axios, setup endpoints, types and api client

---
+++ carnegie-app/src/api/orders.endpoints.ts
@@ -0,0 +1,33 @@
+import apiClient from "./apiClient";
+import { Order, OrderRequest, OrdersResponse } from "./types";
+
+const ordersEndpoints = {
+  getAllOrders: async (): Promise<Order[]> => {
+    try {
+      const { data } = await apiClient.get<OrdersResponse>("/orders");
+      return data.items;
+    } catch (error) {
+      throw new Error("Failed to fetch orders");
+    }
+  },
+  addOrder: async (order: OrderRequest) => {
+    try {
+      const { data } = await apiClient.post<OrderRequest>("/orders", order);
+      console.log("posted data", data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  deleteOrder: async (id: Order["id"]) => {
+    try {
+      const order = await apiClient.delete(`/orders/${id}`);
+      console.log("deleted order", order);
+      return order;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+
+export default ordersEndpoints;
---
+++ carnegie-app/src/api/types.ts
@@ -0,0 +1,29 @@
+export type Action = "buy" | "sell";
+
+export type Order = {
+  id: number;
+  instrumentId: number;
+  amount: number;
+  price: number;
+  action: Action;
+  updatedAt: string;
+  createdAt: string;
+};
+
+export type OrderRequest = {
+  instrumentId: number;
+  amount: number;
+  price: number;
+  action: Action;
+};
+
+export type OrdersResponse = {
+  error: string | null;
+  items: Order[];
+};
+
+export type Instrument = {
+  id: number;
+  ticker: string;
+  name: string;
+};
