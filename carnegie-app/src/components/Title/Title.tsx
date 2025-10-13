c1fde0d662fe3c05c42bd6e7c0512594ed0b8b4d
Minnia <jasminestockholm@gmail.com>
2025-10-13 18:36:53 +0200

    update Title styling

--- carnegie-app/src/components/Title/Title.tsx
+++ carnegie-app/src/components/Title/Title.tsx
@@ -2,7 +2,7 @@ import { Text } from "react-native";
 
 const Title = ({ title }: { title: string | string[] }) => {
   return (
-    <Text style={{ textAlign: "center" }}>
+    <Text style={{ textAlign: "left", margin: 8 }}>
       {Array.isArray(title) ? title.join("  ") : title}
     </Text>
   );
