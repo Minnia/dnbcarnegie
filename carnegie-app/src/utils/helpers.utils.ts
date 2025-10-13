f518deac22337a633bdf4069cd54af42cb2497d9
Minnia <jasminestockholm@gmail.com>
2025-10-13 17:47:04 +0200

    add helper function to find matching instrument

--- carnegie-app/src/utils/helpers.utils.ts
+++ carnegie-app/src/utils/helpers.utils.ts
@@ -1,3 +1,16 @@
+import { Instrument, Order } from "../api/types";
+
 export const hours = (amount: number) => {
   return 1000 * 60 * 60 * amount;
+};
+
+export const findMatchingInstrument = (
+  order: Order,
+  instruments: Instrument[]
+) => {
+  const matching = instruments?.find(
+    (instrument) => order.instrumentId === instrument.id
+  );
+  console.log("Matching result:", matching);
+  return matching;
 };
