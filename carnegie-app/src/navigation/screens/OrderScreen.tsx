c3f99ec3da47b9016b097e5af4b94b1ae2365672
Minnia <jasminestockholm@gmail.com>
2025-10-13 17:47:15 +0200

    wip order screen

--- carnegie-app/src/navigation/screens/OrderScreen.tsx
+++ carnegie-app/src/navigation/screens/OrderScreen.tsx
@@ -1,11 +1,31 @@
+import { Text } from "react-native";
 import ListItem from "../../components/ListItem";
-import { useRoute } from "@react-navigation/native";
+import { RouteProp, useRoute } from "@react-navigation/native";
+import { Order } from "../../api/types";
+import Instrument from "../../components/Instrument";
+
+type OrderScreenParamList = {
+  OrderScreen: {
+    order: Order;
+  };
+};
 
 const OrderScreen = () => {
   const {
     params: { order },
-  } = useRoute<any>();
-  return <ListItem order={order} />;
+  } = useRoute<RouteProp<OrderScreenParamList, "OrderScreen">>();
+  return (
+    <>
+      <Text>Here you can see more details about your order</Text>
+      <Text>{order.id}</Text>
+      <Text>{order.action}</Text>
+      <Text>{order.amount}</Text>
+      <Text>{order.price}</Text>
+      <Text>{order.createdAt}</Text>
+      <Text>{order.updatedAt}</Text>
+      <Instrument order={order} />
+    </>
+  );
 };
 
 export default OrderScreen;
