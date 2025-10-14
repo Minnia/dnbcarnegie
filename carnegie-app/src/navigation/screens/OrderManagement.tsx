76027178cda067c4dfe227924a501b902626548a
Minnia <jasminestockholm@gmail.com>
2025-10-15 00:10:08 +0200

    add order management and move out logic from order screen

--- carnegie-app/src/navigation/screens/OrderManagement.tsx
+++ carnegie-app/src/navigation/screens/OrderManagement.tsx
@@ -1,14 +1,31 @@
-import { Text } from "react-native";
+import { Button, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import useCreateOrder from "../../api/hooks/useCreateOrder";
 
 const OrderManagement = () => {
+  const {
+    mutateAsync: createOrder,
+    error,
+    isPending,
+    isSuccess,
+  } = useCreateOrder();
   return (
     <>
       {/* TODO: make this a common component */}
       <SafeAreaView
         style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
       >
-        <Text>Order Management Screen</Text>
+        <Button
+          title='Add'
+          onPress={() =>
+            createOrder({
+              instrumentId: 1,
+              amount: 100,
+              price: 20,
+              action: "buy",
+            })
+          }
+        />
       </SafeAreaView>
     </>
   );
--- carnegie-app/src/navigation/screens/OrderScreen.tsx
+++ carnegie-app/src/navigation/screens/OrderScreen.tsx
@@ -14,47 +14,7 @@ const OrderScreen = () => {
   const {
     params: { order },
   } = useRoute<RouteProp<OrderScreenParamList, "OrderScreen">>();
-  return (
-    <SafeAreaView style={{ flex: 1, padding: 16 }}>
-      <View style={{ backgroundColor: "red", padding: 16, borderRadius: 8 }}>
-        <Instrument order={order} />
-        <Text style={{ color: "white" }}>Action: {order.action}</Text>
-
-        <Text style={{ color: "white" }}>Price: {order.price} SEK</Text>
-        <Text style={{ color: "white" }}>Amount: {order.amount} st</Text>
-        <Text style={{ color: "white" }}>
-          Total: {(order.price * order.amount).toFixed(2)} SEK
-        </Text>
-        <Text style={{ color: "white" }}>
-          Created:{" "}
-          {new Date(order.createdAt).toLocaleString("sv-SE", {
-            day: "numeric",
-            month: "short",
-          })}
-        </Text>
-        <Text style={{ color: "white" }}>
-          Updated:{" "}
-          {new Date(order.updatedAt).toLocaleString("sv-SE", {
-            day: "numeric",
-            month: "short",
-          })}
-        </Text>
-      </View>
-
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "row",
-          gap: 16,
-        }}
-      >
-        <Button title='Edit Order' onPress={() => {}} />
-        <Button title='Delete Order' onPress={() => {}} />
-      </View>
-    </SafeAreaView>
-  );
+  return <Instrument order={order} />;
 };
 
 export default OrderScreen;
