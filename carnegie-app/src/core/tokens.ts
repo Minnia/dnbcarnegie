6277e9b10e26031fc2028ee7944ed379bdaef7f1
Minnia <jasminestockholm@gmail.com>
2025-10-23 13:40:33 +0200

    add tokens

---
+++ carnegie-app/src/core/tokens.ts
@@ -0,0 +1,20 @@
+const tokens = {
+  BASELINE: 8,
+  ICON: {
+    SMALL: 16,
+    DEFAULT: 24,
+    LARGE: 32,
+    XLARGE: 48,
+  },
+  FONT_SIZE: {
+    SMALL: 12,
+    DEFAULT: 14,
+    LARGE: 16,
+  },
+  FONT_WEIGHT: {
+    NORMAL: "normal",
+    BOLD: "bold",
+  },
+};
+
+export default tokens;
