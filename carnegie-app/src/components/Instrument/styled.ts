b3f9faed5e70947ed755dc8819cfeb0cc02f588a
Minnia <jasminestockholm@gmail.com>
2025-10-23 13:37:27 +0200

    add styling

---
+++ carnegie-app/src/components/Instrument/styled.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/native";
+
+export const InstrumentCard = styled.View`
+  background-color: "#afc39cff";
+  padding: 16px;
+  height: 150px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+`;
+
+export const TextContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  flex: 1;
+`;
