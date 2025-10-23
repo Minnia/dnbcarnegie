6fa8a5a14316b6591f2d87c71467c65857138607
Minnia <jasminestockholm@gmail.com>
2025-10-23 13:37:22 +0200

    update instrument component

--- carnegie-app/src/components/Instrument/Instrument.tsx
+++ carnegie-app/src/components/Instrument/Instrument.tsx
@@ -1,31 +1,34 @@
 import { Alert, Button, Text, View } from "react-native";
 import useGetInstruments from "../../api/hooks/useGetInstruments";
 import { Order } from "../../api/types";
-import { findMatchingInstrument } from "../../utils/helpers.utils";
+import {
+  findMatchingInstrument,
+  formattedDate,
+} from "../../utils/helpers.utils";
 import { SafeAreaView } from "react-native-safe-area-context";
-import useDeleteOrder from "../../api/hooks/useDeleteOrder";
 import { useNavigation } from "@react-navigation/native";
 import useGetOrders from "../../api/hooks/useGetOrders";
 import { Suspense } from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import StickyFooter from "../common/StickyFooter";
+import OrderDetails from "../OrderDetails";
+import * as S from "./styled";
+import { Card } from "../common/styled";
+import { themes } from "../../core/themes";
 
-const Instrument = ({ order }: { order: Order }) => {
+const Instrument = ({
+  order,
+  handleDelete,
+}: {
+  order: Order;
+  handleDelete: () => void;
+}) => {
   const { data: instruments } = useGetInstruments();
   const instrument = findMatchingInstrument(order, instruments || []);
-  const { mutateAsync: deleteOrder } = useDeleteOrder(order.id);
   const { data: orders } = useGetOrders();
-
+  console.log("orders", orders);
   const cachedOrder = orders?.find((o) => o.id === order.id) || order;
-
-  const { navigate, goBack } = useNavigation<any>();
-
-  const handleDelete = async () => {
-    try {
-      await deleteOrder();
-      goBack();
-    } catch (error) {
-      Alert.alert("Error", "Failed to delete the order. Please try again.");
-    }
-  };
+  const { navigate } = useNavigation<any>();
 
   const handleAlert = () => {
     Alert.alert("Confirmation", "Are you sure you want to delete this order?", [
@@ -36,107 +39,80 @@ const Instrument = ({ order }: { order: Order }) => {
 
   return (
     <Suspense fallback={<Text>Loading...</Text>}>
-      <SafeAreaView style={{ flex: 1, padding: 16, top: 50 }}>
-        <View style={{ padding: 16, borderRadius: 8, borderWidth: 1 }}>
-          <View>
-            <Text
-              style={{ fontWeight: "500", textAlign: "center", fontSize: 16 }}
-            >
-              {instrument?.name}
-            </Text>
-            <Text style={{ textAlign: "center", fontSize: 14 }}>
-              {instrument?.ticker}
-            </Text>
+      <SafeAreaView style={{ padding: 16 }}>
+        <ScrollView style={{ height: "100%" }}>
+          <Card
+            backgroundColor='#afc39cff'
+            style={{
+              padding: 16,
+              height: 150,
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 8,
+            }}
+          >
+            <View>
+              <Text
+                style={{ fontWeight: "500", textAlign: "center", fontSize: 16 }}
+              >
+                {instrument?.name}
+              </Text>
+              <Text style={{ textAlign: "center", fontSize: 14 }}>
+                {instrument?.ticker}
+              </Text>
+            </View>
+          </Card>
+          <View style={{ gap: 24, marginTop: 24 }}>
+            <S.TextContainer>
+              <OrderDetails
+                label='Action'
+                value={cachedOrder.action}
+                color={cachedOrder.action === "buy" ? "#008000" : "#ff0000"}
+              />
+              <OrderDetails
+                label='Amount'
+                value={cachedOrder.amount.toString()}
+              />
+            </S.TextContainer>
+
+            <S.TextContainer>
+              <OrderDetails
+                label='Price per unit'
+                value={`${cachedOrder.price} kr`}
+              />
+
+              <OrderDetails
+                label='Total'
+                value={(cachedOrder.price * cachedOrder.amount).toFixed(2)}
+              />
+            </S.TextContainer>
+
+            <S.TextContainer>
+              <OrderDetails
+                label='Created'
+                value={formattedDate(new Date(cachedOrder.createdAt))}
+              />
+              <OrderDetails
+                label='Updated at'
+                value={formattedDate(new Date(cachedOrder.updatedAt))}
+              />
+            </S.TextContainer>
+
+            <StickyFooter>
+              <Button
+                title='Edit Order'
+                onPress={() =>
+                  navigate("Manage order", { order: cachedOrder, instrument })
+                }
+              />
+              <Button
+                title='Delete Order'
+                onPress={handleAlert}
+                color={themes.light.colors.error}
+              />
+            </StickyFooter>
           </View>
-          <Text
-            style={{
-              borderBottomColor: "black",
-              paddingTop: 8,
-              paddingBottom: 2,
-              borderBottomWidth: 1,
-            }}
-          >
-            Action: {cachedOrder.action}
-          </Text>
-
-          <Text
-            style={{
-              borderBottomColor: "black",
-              paddingTop: 8,
-              paddingBottom: 2,
-              borderBottomWidth: 1,
-            }}
-          >
-            Price: {cachedOrder.price} SEK
-          </Text>
-          <Text
-            style={{
-              borderBottomColor: "black",
-              paddingTop: 8,
-              paddingBottom: 2,
-              borderBottomWidth: 1,
-            }}
-          >
-            Amount: {cachedOrder.amount} st
-          </Text>
-          <Text
-            style={{
-              borderBottomColor: "black",
-              paddingTop: 8,
-              paddingBottom: 2,
-              borderBottomWidth: 1,
-            }}
-          >
-            Total: {(cachedOrder.price * cachedOrder.amount).toFixed(2)} SEK
-          </Text>
-          <Text
-            style={{
-              borderBottomColor: "black",
-              paddingBottom: 4,
-              borderBottomWidth: 1,
-            }}
-          >
-            Created:
-            {new Date(cachedOrder.createdAt).toLocaleString("sv-SE", {
-              day: "numeric",
-              month: "short",
-              hour: "2-digit",
-              minute: "2-digit",
-            })}
-          </Text>
-          <Text
-            style={{
-              borderBottomColor: "black",
-              paddingBottom: 4,
-              borderBottomWidth: 1,
-            }}
-          >
-            Updated:
-            {new Date(cachedOrder.updatedAt).toLocaleString("sv-SE", {
-              day: "numeric",
-              month: "short",
-              hour: "2-digit",
-              minute: "2-digit",
-            })}
-          </Text>
-        </View>
-
-        <View
-          style={{
-            flex: 1,
-            paddingTop: 20,
-            justifyContent: "center",
-            flexDirection: "row",
-          }}
-        >
-          <Button
-            title='Edit Order'
-            onPress={() =>
-              navigate("OrderForm", { order: cachedOrder, instrument })
-            }
-          />
-          <Button title='Delete' onPress={handleAlert} />
-        </View>
+        </ScrollView>
       </SafeAreaView>
     </Suspense>
   );
