d881cd7fcad7b674e0d88bfb1a2a846f355e0e8f
Minnia <jasminestockholm@gmail.com>
2025-10-23 13:40:56 +0200

    hook for order form logic

---
+++ carnegie-app/src/forms/useOrderForm.ts
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import useCreateOrder from "../api/hooks/useCreateOrder";
+import useEditOrder from "../api/hooks/useEditOrder";
+import { Action, Instrument, Order } from "../api/types";
+import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
+
+const useOrderForm = (order: Order, instrumentId: Instrument["id"]) => {
+  const editMode = !!order.id;
+
+  console.log("Edit mode:", editMode);
+
+  const { mutateAsync: updateOrder } = useEditOrder(order?.id || 0);
+  const { mutateAsync: createOrder } = useCreateOrder();
+
+  const [amount, setAmount] = useState<string>(order?.amount?.toString() || "");
+  const [price, setPrice] = useState<string>(order?.price?.toString() || "");
+  const [action, setAction] = useState<Action>(order?.action || "buy");
+
+  const { goBack } = useNavigation();
+
+  const handleOrder = async () => {
+    try {
+      if (editMode) {
+        await updateOrder({
+          ...order,
+          amount: parseFloat(amount),
+          price: parseFloat(price),
+          action,
+        });
+        goBack();
+      } else {
+        await createOrder({
+          instrumentId,
+          amount: parseFloat(amount),
+          price: parseFloat(price),
+          action: "buy",
+        });
+        goBack();
+      }
+    } catch (error) {
+      console.error(
+        `Failed to ${editMode ? "update" : "create"} order:`,
+        error
+      );
+      Alert.alert(
+        "Error",
+        `Failed to ${
+          editMode ? "update" : "create"
+        } the order. Please try again.`
+      );
+    }
+  };
+  return {
+    amount,
+    setAmount,
+    price,
+    setPrice,
+    action,
+    setAction,
+    handleOrder,
+    editMode,
+  };
+};
+
+export default useOrderForm;
