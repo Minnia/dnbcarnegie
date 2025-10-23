f45f2ebdc1c9b5280591ed59bde945dd806d6c03
Minnia <jasminestockholm@gmail.com>
2025-10-23 13:35:13 +0200

    add sticky footer

---
+++ carnegie-app/src/components/common/StickyFooter/StickyFooter.tsx
@@ -0,0 +1,27 @@
+import { View, ViewStyle } from "react-native";
+import { ReactNode } from "react";
+import { Container } from "../styled";
+import { themes } from "../../../core/themes";
+
+interface StickyFooterProps {
+  children: ReactNode;
+  style?: ViewStyle;
+}
+
+const StickyFooter = ({ children, style }: StickyFooterProps) => {
+  return (
+    <Container
+      backgroundColor='transparent'
+      justifyContent='center'
+      alignItems='flex-end'
+      flexDirection='row'
+      style={{
+        ...style,
+      }}
+    >
+      {children}
+    </Container>
+  );
+};
+
+export default StickyFooter;
