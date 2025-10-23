2edf1f444a3374cb46ecf9bac5845f59fa9dde83
Minnia <jasminestockholm@gmail.com>
2025-10-23 13:40:46 +0200

    move order form logic to separate hook

--- carnegie-app/src/forms/OrderForm.tsx
+++ carnegie-app/src/forms/OrderForm.tsx
@@ -1,9 +1,17 @@
-import { Alert, Button, Text, TextInput, View } from "react-native";
-import { Action, Instrument, Order } from "../api/types";
-import useEditOrder from "../api/hooks/useEditOrder";
-import { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
-import useCreateOrder from "../api/hooks/useCreateOrder";
+import { TextInput, View } from "react-native";
+import { Instrument, Order } from "../api/types";
+import { SafeAreaView } from "react-native-safe-area-context";
+import useOrderForm from "./useOrderForm";
+
+import {
+  Card,
+  StyledText,
+  Container,
+  Spacer,
+} from "../components/common/styled";
+import tokens from "../core/tokens";
+import TextSwitch from "../components/common/Switch/Switch";
+import Button from "../components/common/Button";
 
 const OrderForm = ({
   order,
@@ -12,67 +20,104 @@ const OrderForm = ({
   order?: Order;
   instrument: Instrument;
 }) => {
-  const editMode = !!order;
+  const {
+    amount,
+    setAmount,
+    price,
+    setPrice,
+    handleOrder,
+    editMode,
+    action,
+    setAction,
+  } = useOrderForm(order || ({} as Order), instrument.id);
 
-  const { mutateAsync: updateOrder } = useEditOrder(order?.id || 0);
-  const { mutateAsync: createOrder } = useCreateOrder();
-
-  const [amount, setAmount] = useState<string>(order?.amount?.toString() || "");
-  const [price, setPrice] = useState<string>(order?.price?.toString() || "");
-
-  const { navigate, goBack } = useNavigation();
-
-  const handleOrder = async () => {
-    try {
-      if (editMode) {
-        await updateOrder({
-          ...order,
-          amount: parseFloat(amount),
-          price: parseFloat(price),
-        });
-        goBack();
-      } else {
-        await createOrder({
-          instrumentId: instrument.id,
-          amount: parseFloat(amount),
-          price: parseFloat(price),
-          action: "buy",
-        });
-        goBack();
-      }
-    } catch (error) {
-      console.error(
-        `Failed to ${editMode ? "update" : "create"} order:`,
-        error
-      );
-      Alert.alert(
-        "Error",
-        `Failed to ${
-          editMode ? "update" : "create"
-        } the order. Please try again.`
-      );
-    }
-  };
   return (
-    <View>
-      <Text>{instrument?.name}</Text>
-      <TextInput
-        placeholder={order?.amount?.toString() ?? "Amount"}
-        keyboardType='numeric'
-        onChangeText={(text) => setAmount(text)}
-        onBlur={() => setAmount(amount)}
-      />
-      <TextInput
-        placeholder={order?.price?.toString() ?? "Price"}
-        keyboardType='numeric'
-        onChangeText={(text) => setPrice(text)}
-        onBlur={() => setPrice(price)}
-      />
-      <Button
-        title={editMode ? "Update order" : "Create order"}
-        onPress={handleOrder}
-      />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        padding: tokens.BASELINE * 2,
+        justifyContent: "center",
+      }}
+    >
+      <View>
+        <StyledText fontSize={20} fontWeight='bold'>
+          {instrument?.name}
+        </StyledText>
+        <Spacer size={16} />
+        <StyledText fontSize={tokens.FONT_SIZE.DEFAULT} fontWeight='bold'>
+          Amount of unit
+        </StyledText>
+        <TextInput
+          style={{
+            borderWidth: 1,
+            borderRadius: tokens.BASELINE,
+            height: 40,
+            paddingHorizontal: tokens.BASELINE,
+          }}
+          placeholder={order?.amount?.toString() ?? "Amount"}
+          keyboardType='numeric'
+          onChangeText={(text) => setAmount(text)}
+          onBlur={() => setAmount(amount)}
+        />
+        <Spacer size={16} />
+        <StyledText fontSize={tokens.FONT_SIZE.DEFAULT} fontWeight='bold'>
+          Price of unit
+        </StyledText>
+        <TextInput
+          style={{
+            borderWidth: 1,
+            borderRadius: tokens.BASELINE,
+            height: 40,
+            paddingHorizontal: tokens.BASELINE,
+          }}
+          placeholder={order?.price?.toString() ?? "Price"}
+          keyboardType='numeric'
+          onChangeText={(text) => setPrice(text)}
+          onBlur={() => setPrice(price)}
+        />
+        <Spacer size={4} />
+        <StyledText fontSize={10}>Price in SEK</StyledText>
+        <Spacer size={32} />
+        {editMode && (
+          <>
+            <Container
+              alignItems='center'
+              flexDirection='row'
+              justifyContent='center'
+            >
+              <TextSwitch
+                value={action === "buy"}
+                onValueChange={() =>
+                  setAction(action === "buy" ? "sell" : "buy")
+                }
+                leftText='Buy'
+                rightText='Sell'
+              />
+            </Container>
+            <Spacer size={32} />
+          </>
+        )}
+        <Card backgroundColor='#afc39cff' style={{ padding: 16 }}>
+          <StyledText fontSize={tokens.FONT_SIZE.DEFAULT} fontWeight='bold'>
+            Total order value
+          </StyledText>
+          <StyledText>{Number(price) * Number(amount)} kr</StyledText>
+        </Card>
+        <Spacer size={32} />
+        <Container
+          flexDirection='row'
+          justifyContent='center'
+          alignItems='center'
+          height={50}
+        >
+          <Button
+            variant='secondary'
+            title={editMode ? "Update order" : "Create order"}
+            onPress={handleOrder}
+          />
+        </Container>
+      </View>
+    </SafeAreaView>
   );
 };
 
