4118afb16e986b7b56e3fc6cb5c2624a8d920fd5
Minnia <jasminestockholm@gmail.com>
2025-10-25 21:01:30 +0200

    remove code that shouldn't be in App.tsx

--- carnegie-app/App.tsx
+++ carnegie-app/App.tsx
@@ -1,25 +1,15 @@
 import "react-native-gesture-handler";
-import { Dimensions } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./src/integrations";
 import { MainStack } from "./src/navigation/stacks/MainStack";
 import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-  const dimensions = Dimensions.get("screen");
   return (
     <>
       <NavigationContainer>
         <QueryClientProvider client={queryClient}>
-          <SafeAreaView
-            style={{
-              flex: 1,
-              width: dimensions.width,
-            }}
-          >
-            <MainStack />
-          </SafeAreaView>
+          <MainStack />
         </QueryClientProvider>
       </NavigationContainer>
     </>
