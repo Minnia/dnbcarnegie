12d75a06b4874638e5f4026cf51e537dc33add3c
Minnia <jasminestockholm@gmail.com>
2025-10-25 21:12:50 +0200

    add icons for bottom tab bar

--- carnegie-app/src/navigation/stacks/BottomTabNavigation.tsx
+++ carnegie-app/src/navigation/stacks/BottomTabNavigation.tsx
@@ -1,17 +1,36 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { OrderStack } from "./OrderStack";
 import { OrderManagementStack } from "./OrderManagementStack";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export const BottomTabNavigation = () => {
   const { Navigator, Screen } = createBottomTabNavigator();
   return (
-    <Navigator initialRouteName='Orders'>
+    <Navigator
+      initialRouteName='Orders'
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Orders") {
+            iconName = focused ? "list-circle" : "list-circle-outline";
+          } else if (route.name === "Create order") {
+            iconName = focused ? "card" : "card-outline";
+          }
+          return <Ionicons name={iconName as any} size={size} color={color} />;
+        },
+      })}
+    >
       <Screen
         name='Orders'
         options={{ headerShown: false }}
         component={OrderStack}
       />
-      <Screen name='Create order' component={OrderManagementStack} />
+      <Screen
+        name='Create order'
+        options={{ headerShown: false }}
+        component={OrderManagementStack}
+      />
     </Navigator>
   );
 };
